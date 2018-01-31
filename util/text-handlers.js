7b53a8daa193a961cb2ccfa4fc1fc5791fd9c0eb
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-01-31 14:03:36 +0300

    Fix breaked tags

--- util/text-handlers.js
+++ util/text-handlers.js
@@ -1,5 +1,17 @@
 const PAGE_LENGHT = 600
 
+const findBreakTag = str => {
+  let endRegExp = /(<(\/??)(\w+)[\s.*]?>)[^>]*?$/
+  let startRegExp = /^.*?[^<]?(<(\/??)(\w+).*?>)/
+  
+  matchEnd = endRegExp.exec(str)
+  matchStart = startRegExp.exec(str)
+  
+  if (matchEnd[2] !== '/') str += `</${matchEnd[3]}>`
+  if (matchStart[2] === '/') str = `<${matchStart[3]}>` + str
+  return str
+}
+
 const getMaxPage = (text) => 
     Math.floor(text.split('').length / PAGE_LENGHT)
 
@@ -11,25 +23,27 @@ const getPart = (text) => {
   for (let i = 0; i <= partsCount; i++) {
     let count = wordArray.length / partsCount
     let part = wordArray
-    .slice( i < 1 ? 0 : count*(i), count*(i + 1))
-    .join(' ')
+      .slice( i < 1 ? 0 : count*(i), count*(i + 1))
+      .join(' ')
     parts.push(part)
   }
   return parts
 }
 
 const getText = (text, number) => {
-  let part = getPart(text)
-  return part[(number - 1)]
+  let parts = getPart(text)
+  part = parts[(number - 1)]
+  console.log(findBreakTag(part))
+  return findBreakTag(part)
 }
 
 const getString = text => {
   let brRegExp = /(<br\/>)+/g
   let breakRegExp = /(\n)+/g  
   return text
-  .replace(brRegExp, '¶')
-  .replace(breakRegExp, ' ¶ ')
-  .replace(/<\/?p>/g, ' ')
+    .replace(brRegExp, '¶')
+    .replace(breakRegExp, ' ¶ ')
+    .replace(/<\/?p>/g, ' ')
 }
 
 module.exports = {
